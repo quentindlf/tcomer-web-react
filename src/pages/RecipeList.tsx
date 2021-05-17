30f46d4a076017e49669df0ea44ccbd8226c2e36
Quentin DLF <quentin7611@yahoo.fr>
2021-05-17 17:13:31 -0400

    Fetch from server

--- src/pages/RecipeList.tsx
+++ src/pages/RecipeList.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
-import { mockRecipe } from "../mocks/recipe.mock";
+import recipeApi from "../api/recipeApi";
+import { mockRecipe, RecipeModel } from "../mocks/recipe.mock";
 
 const RecipeList = () => {
-  const recipes = [mockRecipe, mockRecipe];
+  const [recipes, setRecipes] = useState<RecipeModel[]>([]);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async () => {
+    const response = await recipeApi.get("/recipes");
+    setRecipes(response.data);
+  };
 
   return (
     <>
@@ -12,7 +22,9 @@ const RecipeList = () => {
       <ListGroup>
         {recipes.map((recipe) => (
           <ListGroup.Item action>
-            <Link to="/recipe-details" className="nav-link">{recipe.title}</Link>
+            <Link to="/recipe-details" className="nav-link">
+              {recipe.title}
+            </Link>
           </ListGroup.Item>
         ))}
       </ListGroup>
